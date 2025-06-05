acec475d7ade6238f3e374cabf069e09367a44f5
WesAllenInc <jake@wesallencompany.com>
2025-06-05 16:52:30 -0400

    feat: add utility functions for class merging and currency formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Format a number as currency (USD)
+ * @param value - The number to format
+ * @param options - Intl.NumberFormat options
+ * @returns Formatted currency string
+ */
+export function formatCurrency(value: number, options?: Intl.NumberFormatOptions) {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    ...options,
+  });
+  
+  return formatter.format(value);
+}

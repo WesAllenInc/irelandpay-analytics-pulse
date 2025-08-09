6ac80a53cd8e2febb89b6e11e8d623fa763d67fb
WesAllenInc <jake@wesallencompany.com>
2025-08-09 15:52:23 -0400

    chore(types): standardize Supabase DB types to UTF-8 at types/database.ts; add script; update imports; remove old UTF-16 file

--- components/luma-spin.tsx
+++ components/luma-spin.tsx
@@ -1,6 +1,6 @@
 export const Component = () => {
 return (
-    <div className= "relative w-[65px] aspect-square" >
+    <div className="relative w-[65px] aspect-square">
     <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
       <span className="absolute rounded-[50px] animate-loaderAnim animation-delay shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
         <style jsx > {`
@@ -40,6 +40,6 @@ return (
           animation-delay: -1.25s;
         }
       `}</style>
-  < /div>
+    </div>
   );
 };
